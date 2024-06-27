0f8a6704a4556963fe7d8497b854c1bd7acf5995
breadpandedpan <krisha.agarwal@csedge.org>
2024-06-27 15:00:15 -0700

    V2

--- pass.py
+++ pass.py
@@ -7,27 +7,35 @@ password = list(password)
 # vars for symbol check while loop
 symbols = ["!", "@", "#", "$", "%", "^", "&", "*"]
 i = 0
-val = 7
+val = True
 
 if 8 < len(password) < 15: #check len
 
     # check symbols
-    while val >= 7:
+    while val:
         if i >= len(symbols):
             print("reject")
-            exit()
+            val = False
         elif symbols[i] in password:
-            password.pop(i)
-            print(password)
-            val += 1
             print("accept")
-
+            val = False
         else:
             i += 1
 
-    "".join(password) #turns password into str to do below funcs
-    if password.isalnum():  #Returns True if all characters in the string are alphanumeric
-        if password.isupper() and password.islower() == False: # if mix of upper & lower --> False; else one would be True
-            print("Accept!")
+        num = 0
+        something = True
+        while something:
+            if symbols[num] in password:
+                password.pop(num)
+                num += 2
+            else:
+                num += 1
+            
+
+
+        "".join(password)
+        if password.isalnum():  #Returns True if all characters in the string are alphanumeric
+            if password.isupper() and password.islower() == False: # if mix of upper & lower --> False; else one would be True
+                print("Accept!")
 else:
     print("Reject")
